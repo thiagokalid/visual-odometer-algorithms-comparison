d49fc99cfe1ee50178bc3c7172d36c477c05d019
Thiago Kalid <thiagokkalid@gmail.com>
2025-08-21 13:34:53 -0300

    initial project structure

---
+++ scripts/dataset_creation.py
@@ -0,0 +1,7 @@
+"""
+O movimento realizado pelo encoder foi aproximadamente:
+1. 110 mm na horizontal para a direita ➡️
+2. 120 mm na vertical para baixo ⬇️
+
+Para conversão de pixels para mm a taxa correta é de 22.2 px/mm.
+"""
