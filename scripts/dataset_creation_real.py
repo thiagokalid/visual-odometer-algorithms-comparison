617e86a1b2fe2eb206c93fc88e2aebec52c4fcd2
Thiago Kalid <thiagokkalid@gmail.com>
2025-09-27 15:54:03 -0300

    scripts to create dataset

---
+++ scripts/dataset_creation_real.py
@@ -0,0 +1,54 @@
+"""
+O movimento realizado pelo encoder foi aproximadamente:
+1. 110 mm na horizontal para a direita ➡️
+2. 120 mm na vertical para baixo ⬇️
+
+Para conversão de pixels para mm a taxa correta é de 22.2 px/mm.
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from PIL import Image, ImageOps
+
+
+def load_img(filename):
+    img_array_rgb = Image.open(filename)
+    img_grayscale = ImageOps.grayscale(img_array_rgb)
+    return np.array(img_grayscale)
+
+inspections = os.listdir("../data/")
+inspections.remove(".gitkeep")
+inspections.remove("PIPE_IMG.jpg")
+inspections.remove("pipe_imgs")
+inspections = [inspection for inspection in inspections if not inspection.endswith(".pkl")]
+
+col1, col2, col3, col4 = [], [], [], []
+header = ["inspection", "filename", "order", "img"]
+
+data = {
+    "inspection": [],
+    "filename": [],
+    "order": [],
+    "img": [],
+    "delta_x": [],
+    "delta_y": [],
+}
+
+for inspection in inspections:
+    imgs_path = "../data/" + inspection + "/"
+    imgs_name = os.listdir(imgs_path)
+    imgs_name = [name for name in imgs_name if not name.endswith(".csv")]
+    imgs_name.sort()
+
+    data['order'].extend(np.array(range(len(imgs_name))))
+    data['filename'].extend(imgs_name)
+    data['inspection'].extend([inspection] * len(imgs_name))
+    data['img'].extend([load_img(imgs_path + img_name) for img_name in imgs_name])
+    data['delta_x'].extend(len(imgs_name) * [0.0])
+    data['delta_y'].extend(len(imgs_name) * [0.0])
+
+data['delta_x'][-1] = 22.2 * 110
+data['delta_y'][-1] = 22.2 * 120
+dataset_df = pd.DataFrame(data)
+dataset_df.to_pickle("../data/dataset.pkl")
