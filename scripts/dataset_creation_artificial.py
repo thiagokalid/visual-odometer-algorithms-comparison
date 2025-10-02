5852554b184bff44a9cd9a175b423b224c107c5f
Thiago Kalid <thiagokkalid@gmail.com>
2025-10-01 21:50:01 -0300

    Final update

--- scripts/dataset_creation_artificial.py
+++ scripts/dataset_creation_artificial.py
@@ -1,13 +1,12 @@
-import matplotlib
 import numpy as np
 import cv2
 from scipy.fft import fft2, ifft2
-from scipy.signal.windows import tukey
 import matplotlib.pyplot as plt
-from math import ceil
-import matplotlib
+import os
 import pandas as pd
-matplotlib.use('TkAgg')
+
+
+# matplotlib.use('QtAgg')
 
 def fft_shift_2d(imagem, dx, dy, janela=None):
     h, w = imagem.shape
@@ -24,11 +23,13 @@ def fft_shift_2d(imagem, dx, dy, janela=None):
     I_deslocada = ifft2(I_fft * fase)
     return np.real(I_deslocada)
 
+
 def add_gaussian_noise(img, sigma=10):
     noise = np.random.normal(0, sigma, img.shape)
     return img + noise
 
-def add_salt_and_pepper(img, amount=90/100, s_vs_p=0.5):
+
+def add_salt_and_pepper(img, amount=90 / 100, s_vs_p=0.5):
     noisy = img.copy()
     num_salt = np.ceil(amount * img.size * s_vs_p)
     coords = (np.random.randint(0, img.shape[0], int(num_salt)),
@@ -40,6 +41,7 @@ def add_salt_and_pepper(img, amount=90/100, s_vs_p=0.5):
               np.random.randint(0, img.shape[1], int(num_pepper)))
     noisy[coords] = 0
     return noisy
+
 
 def apply_lens_blur(img, blur_kernel=(21, 21), feather=100):
     rows, cols = img.shape[:2]
@@ -60,9 +62,15 @@ def apply_full_blur(img, blur_kernel=(7, 7)):
     """
     return cv2.GaussianBlur(img, blur_kernel, 0)
 
+
 # Exemplo de uso:
 
 DATA_ROOT = "../data/pipe_imgs/"
+if not os.path.exists(DATA_ROOT):
+    os.makedirs(DATA_ROOT)
+    print(f"Directory created: {DATA_ROOT}")
+else:
+    print(f"Directory already exists: {DATA_ROOT}")
 
 base_img = cv2.cvtColor(cv2.imread("../data/PIPE_IMG.jpg"), cv2.COLOR_BGR2GRAY)
 
@@ -79,38 +87,38 @@ trajectory_description = [
     {
         "shift_x": 1000, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.5, "shift_noise_y": 0.1
-     },
+    },
     {
         "shift_x": 0, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.05, "shift_noise_y": 0.05
-     },
+    },
     {
         "shift_x": 0, "shift_y": 500,
         "N": 100, "shift_noise_x": 0.1, "shift_noise_y": 0.5
-     },
+    },
     {
         "shift_x": 0, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.05, "shift_noise_y": 0.05
-     },
+    },
     {
         "shift_x": -1000, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.5, "shift_noise_y": 0.1
-     },
+    },
     {
         "shift_x": 0, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.05, "shift_noise_y": 0.05
-     },
+    },
     {
         "shift_x": 0, "shift_y": -500,
         "N": 100, "shift_noise_x": 0.1, "shift_noise_y": 0.5
-     },
+    },
     {
         "shift_x": 0, "shift_y": 0,
         "N": 100, "shift_noise_x": 0.05, "shift_noise_y": 0.05
     }
 ]
 
-x0, z0 = img_size[1]//2 + 20, img_size[0]//2 + 20
+x0, z0 = img_size[1] // 2 + 20, img_size[0] // 2 + 20
 
 data = {
     "inspection": [],
@@ -127,11 +135,11 @@ imgs = []
 
 total_iter = np.sum([description["N"] for description in trajectory_description])
 
-curr_iter= 0
+curr_iter = 0
 for description in trajectory_description:
     shift_x, shift_y = description["shift_x"], description["shift_y"]
     N = description["N"]
-    delta_x, delta_y = shift_x/N, shift_y/N
+    delta_x, delta_y = shift_x / N, shift_y / N
 
     for i in range(1, N):
         rand_delta_x = np.random.normal(delta_x, description["shift_noise_x"])
@@ -144,13 +152,14 @@ for description in trajectory_description:
         true_shift_y.append(rand_delta_y)
 
         int_x, int_z = np.round(x[-1]), np.round(z[-1])
-        img_int = base_img[int(int_z - img_size[0]//2) : int(int_z + img_size[0]//2), int(int_x - img_size[1]//2) : int(int_x + img_size[1]//2)]
+        img_int = base_img[int(int_z - img_size[0] // 2): int(int_z + img_size[0] // 2),
+                  int(int_x - img_size[1] // 2): int(int_x + img_size[1] // 2)]
         float_x, float_z = x[-1] - int_x, z[-1] - int_z
         shifted_img = fft_shift_2d(img_int, dx=float_x, dy=float_z)
 
         shifted_img = apply_lens_blur(shifted_img)
         shifted_img = apply_full_blur(shifted_img)
-        shifted_img = add_gaussian_noise(shifted_img, sigma=30)
+        shifted_img = add_gaussian_noise(shifted_img, sigma=15)
         # shifted_img = add_salt_and_pepper(shifted_img, amount=0.01)
 
         shifted_img = np.clip(shifted_img, 0, 255).astype(np.uint8)
@@ -176,6 +185,3 @@ plt.figure()
 plt.plot(x, z, 'o')
 plt.axis("equal")
 plt.show()
-
-
-
